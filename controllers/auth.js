78c46b762b98c0ec52eaaeae64071ce01a71a19c
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-03-31 22:28:59 -0500

    Crear un usuario personalizado con las credenciales de Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcryptjs = require("bcryptjs");
 const { response } = require("express");
 const { jwtGenerate } = require("../helpers/generate-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 const User = require('../models/user')
 
 const login = async(req,res = response) => {
@@ -36,10 +37,48 @@ const googleSignIn = async (req, res = response)=> {
 
     const { id_token } = req.body
 
-    return res.status(200).json({
-        msg:'todo ok',
-        id_token
-    })
+    try {
+
+        const { name,email,img} = await googleVerify( id_token )
+
+        let user = await User.findOne({email: email})
+
+        if(! user){
+
+            const data = {
+               name,
+               email,
+               img,
+               password: ':P',
+               google:true,
+               role:'USER_ROLE'
+            }
+
+            user = new User(data)
+            await user.save()
+        }
+
+        if( ! user.status){
+            return response.status(401).json({
+                msg:'El usuario se encuentra bloqueado - status:false'
+            })
+        }
+
+        //generar JWT
+        const token = await jwtGenerate(user.id)   
+
+
+        res.status(200).json({
+           user,
+           token
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            msg:'El token  de google no es válido'
+        })
+    }
+
 }
 
 
